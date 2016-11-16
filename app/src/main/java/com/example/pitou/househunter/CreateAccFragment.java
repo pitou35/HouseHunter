7aa167bd21ff6eb9f7e6a7be94d4a7c853be409c
Pierre-AUgustin TOTARO <pierreaugustin.totaro03@gmail.com>
2016-11-16 18:14:51 +0100

    auth

--- app/src/main/java/com/example/pitou/househunter/CreateAccFragment.java
+++ app/src/main/java/com/example/pitou/househunter/CreateAccFragment.java
@@ -4,12 +4,14 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -18,17 +20,66 @@ import com.google.firebase.auth.FirebaseAuth;
 
  */
 public class CreateAccFragment extends Fragment {
+
+    private EditText inputEmail, inputPassword;
+    private Button btnSignIn, btnSignUp, btnResetPassword;
+    private ProgressBar progressBar;
+    private FirebaseAuth auth;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
         // Defines the xml file for the fragment
-        return inflater.inflate(R.layout.fragment_create_acc, parent, false);
+
+        View view= inflater.inflate(R.layout.fragment_create_acc, parent, false);
+        btnSignIn = (Button) view.findViewById(R.id.sign_in_button);
+        btnSignUp = (Button) view.findViewById(R.id.sign_up_button);
+        inputEmail = (EditText) view.findViewById(R.id.email);
+        inputPassword = (EditText) view.findViewById(R.id.password);
+        progressBar = (ProgressBar) view.findViewById(R.id.progressBar);
+        btnResetPassword = (Button) view.findViewById(R.id.btn_reset_password);
+
+        btnResetPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO:onclick
+            }
+        });
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO:finish
+            }
+        });
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = inputEmail.getText().toString().trim();
+                String password = inputPassword.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(getContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(getContext(), "Enter password!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (password.length() < 6) {
+                    Toast.makeText(getContext(), "Password too short, enter minimum 6 characters!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                progressBar.setVisibility(View.VISIBLE);
+                
+            }
+        });
+
+        return view;
     }
 
-    // This event is triggered soon after onCreateView().
-    // Any view setup should occur here.  E.g., view lookups and attaching view listeners.
-    @Override
-    public void onViewCreated(View view, Bundle savedInstanceState) {
-        // Setup any handles to view objects here
-        // EditText etFoo = (EditText) view.findViewById(R.id.etFoo);
-    }
+
 }
