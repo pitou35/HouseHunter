785d78e7d9ff10cfc3079def3c26a145a5893e7a
SandraIsabel <sandra.isabel.villafuerte@gmail.com>
2016-12-01 10:58:06 +0100

    detail de l'annonce

--- app/src/main/java/com/example/pitou/househunter/DetailAnnonceFragment.java
+++ app/src/main/java/com/example/pitou/househunter/DetailAnnonceFragment.java
@@ -4,105 +4,98 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import com.example.pitou.househunter.Adapters.AnnoncesAdapter;
+import com.example.pitou.househunter.model.Annonce;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import static com.google.android.gms.internal.zzs.TAG;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link DetailAnnonceFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link DetailAnnonceFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class DetailAnnonceFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+   //elements de l'interface
+    private EditText titreAnnonce;
+    private EditText descriptionAnnonce;
+    private EditText adresse;
+    private EditText prixLogement;
+    private EditText telContact;
+    private EditText disponibilite;
 
-    private OnFragmentInteractionListener mListener;
+
+    //conexion bd
+    private FirebaseDatabase db;
+    private FirebaseAuth auth;
+    private DatabaseReference myRef;
+    private String idAnnonce;
 
     public DetailAnnonceFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment DetailAnnonceFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static DetailAnnonceFragment newInstance(String param1, String param2) {
-        DetailAnnonceFragment fragment = new DetailAnnonceFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail_annonce, container, false);
+
+        db = FirebaseDatabase.getInstance();
+        auth = FirebaseAuth.getInstance();
+        auth.getCurrentUser();
+        myRef = db.getReference("Annonces");
+
+
+        View view = inflater.inflate(R.layout.fragment_detail_annonce, container, false);
+
+        titreAnnonce = (EditText) view.findViewById(R.id.txtTitre);
+        descriptionAnnonce = (EditText) view.findViewById(R.id.txtDescription);
+        adresse = (EditText) view.findViewById(R.id.txtAdresse);
+        prixLogement = (EditText) view.findViewById(R.id.txtPrix);
+        telContact = (EditText) view.findViewById(R.id.txtContact);
+        disponibilite = (EditText) view.findViewById(R.id.txtDisponibilite);
+
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                for (DataSnapshot child : dataSnapshot.getChildren()){
+                    Log.i(TAG, "Value is: " + child.getValue(Annonce.class).getTitre());
+                    Annonce value = child.getValue(Annonce.class);
+                    if(child.getKey().equals("-KXGT9i-36bXX5DCCApg")){
+                        titreAnnonce.setText(value.getTitre());
+                        descriptionAnnonce.setText(value.getDescription());
+                        prixLogement.setText(value.getPrixLogment());
+                        telContact.setText(value.getTelContact());
+                        disponibilite.setText(value.getDisponibiliteLogement());
+                        adresse.setText(value.getAdresse());
+                    }
+
+
+                }
+
+
+            }
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+
+        return view;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }
 }
